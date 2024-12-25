e53aba7fb26d2939345d92cd2ef397bd7edfba3e
MuXaPRoDuCTioN <62043770+MuXaPRoDuCTioN@users.noreply.github.com>
2024-12-25 13:49:41 +0700

    Add files via upload

---
+++ Main_Menu.cpp
@@ -0,0 +1,14 @@
+#include "Main_Menu.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	InfinityDungeons::Main_Menu form;
+	//Старт программы
+	Application::Run(% form);
+	return 0;
+}
